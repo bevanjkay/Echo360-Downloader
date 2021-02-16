398d2fb8b74383431f4cc2808646a78b723e4abb
Bevan Kay <bevanjkay@gmail.com>
2021-02-16 16:32:16 +1100

    Batch downloading

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ A tool for downloading Echo360 videos.
 - Run `npm install` in the cloned directory.
 - Modify the `url` variable in `index.js` to the URL of the video you want to download.
 - Run `node index.js`.
+
+## Optional
+To download a batch of videos, enter the URLs or IDs into the file `inputs.txt`, one per line.
+When you run `node index.js` you will be prompted whether you would like to use the provided data from the `inputs.txt` file.
+If you respond with `n`, you will be prompted to input a URL or ID to continue.
--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 	http://www.apache.org/licenses/LICENSE-2.0
 */
 
-
 import FS from 'fs';
 import Got from 'got';
 import {CookieJar} from 'tough-cookie';
@@ -12,30 +11,87 @@ import * as M3U8Parser from 'm3u8-parser';
 import FFmpeg from 'fluent-ffmpeg';
 import ffmpegPath from 'ffmpeg-static';
 import prompt from 'prompt';
+import async from 'async';
+import progress from 'cli-progress';
+import { inherits } from 'util';
 
+
+prompt.start();
+
+let urls;
+let title;
 
 let url = ''; // The URL of the desired video.
+FS.readFile('inputs.txt', 'utf8', function (err, data) {
+	urls = data ? data.split('\n') : [];
+	console.log(`${urls.length} videos in input file.`)
+	console.log(urls.join('\n'));
+	init(urls);
+})
+
 const outputFolder = 'Output';
 
-const ffmpeg = new FFmpeg();
 const cookieJar = new CookieJar();
 
-const promptSchema = {
+const urlPromptSchema = {
     properties: {
       url: {
-        message: 'Please enter URL or ID of video',
+        description: 'Please enter URL or ID of video',
         required: true
       },
     }
   };
 
-prompt.start();
+const fileSchema = {
+	properties: {
+		file: {
+			description: `Input file(s) found – Use this input (y/n)?`,
+			message: 'Please enter y or n',
+			pattern: /y|n/,
+			required: true,
+		}
+	}
+}
 
-prompt.get(promptSchema, function (err, result) {
-	if (err) { console.log(err)}
-	url = result.url;
-	main();
-});
+function init(data) {
+	if (data.length) {
+	prompt.get(fileSchema, function (err, result) {
+		if (err) { console.log(err) }
+		if (result.file === 'y') {
+			async.eachSeries(urls, function(asyncUrl, next) {
+				url = asyncUrl;
+				return main(asyncUrl, next);
+			}, function (err)  {
+				if (err) {
+					console.loge(err)
+				} else {
+					return done(null)
+				}});	
+		} else {
+			getUrl();
+		}
+	})
+	} else {
+		getUrl();
+	}
+}
+
+
+function done(err) {
+	if (err) console.log(err);
+	if (!err) console.log('All files processed successfully.');
+	process.exit();
+}
+
+
+
+function getUrl() {
+	prompt.get(urlPromptSchema, function (err, result) {
+		if (err) { console.log(err)}
+		url = result.url;
+		main(url, done);
+	});
+};
 
 // Extracts a substring from between the two given marker strings.
 function extract(source, startMarker, endMarker)
@@ -66,35 +122,39 @@ async function saveStream(type, playlist, m3u8Url)
 
 	// Save the stream.
 	if(!FS.existsSync(outputFolder)) FS.mkdirSync(outputFolder);
-	const filePath = `${outputFolder}/${fileName}`;
+	const filePath = `${outputFolder}/${title}-${type}`;
 	FS.writeFileSync(filePath, response.rawBody);
 
 	return filePath;
 }
 
 
+
+
 // Main.
-async function main()
+async function main(asyncUrl, next)
 {
 
-	if (!/^http/.test(url)) {
-		url = `https://echo360.org.au/media/${url}/public`
+	const ffmpeg = new FFmpeg();
+
+	if (!/^http/.test(asyncUrl)) {
+		url = `https://echo360.org.au/media/${asyncUrl}/public`
 	}
 
-	console.log(`Attempting to download video from ${url}...`)
+	console.log(`Attempting to download video from ${asyncUrl}...`)
 
 	try
 	{
 		// Get the cookies and keys.
-		const indexResponse = await Got(url, {cookieJar});
+		const indexResponse = await Got(asyncUrl, {cookieJar});
 		const data = JSON.parse(extract(indexResponse.body,
 			`Echo["mediaPlayerApp"]("`, `");`).replace(/\\/g, ''));
 
 		const regex = /(?<=<title>).+(?=<\/title>)/
-		const title = indexResponse.body.match(regex)[0].replace('.mp4', '');
+		title = indexResponse.body.match(regex)[0].replace('.mp4', '') || 'Video';
 
 		console.log(`Found video with title ${title}...`)
-
+		
 		// Get the M3U8.
 		const m3u8Url = data.sources.video1.source;
 		const m3u8Response = await Got(m3u8Url, {cookieJar});
@@ -110,25 +170,65 @@ async function main()
 		const audioFilePath = await saveStream('audio', parsedM3u8.playlists[2], m3u8Url);
 
 		// Merge the audio and video streams.
-		console.log('Merging...');
 
-		await new Promise((resolve) => ffmpeg
+		// Initiate progress bar
+		const progressBar = new progress.SingleBar({
+			format: 'Merging [{bar}] {percentage}%'
+		}, progress.Presets.legacy);
+
+		progressBar.start(100, 0);
+
+		// Run FFmpeg
+		await new Promise(resolve => ffmpeg
 			.setFfmpegPath(ffmpegPath)
 			.input(videoFilePath)
 			.input(audioFilePath)
+			.on('progress', function (pro) {
+				progressBar.update(Math.round(pro.percent));
+			})
+			.output( `${outputFolder}/${title}.mp4`, {end: true})
 			.on('end', resolve)
-			.save(`${outputFolder}/${title}.mp4`));
+			.run());
 
-		// Delete the stream files.
-		FS.unlinkSync(audioFilePath);
-		FS.unlinkSync(videoFilePath);
+		// Remove created temporary files
+		await new Promise(resolve => {
+			console.log('Completed save...');
+			
+			FS.unlink(audioFilePath, () => {
+				console.log('Removed audio stream file...')
+			});
 
-		console.log('Done.');
+			FS.unlink(videoFilePath, () => {
+				console.log('Removed audio stream file...')
+				console.log('Finished...')
+
+				resolve();
+			});
+		})
+
+		// Process Next File
+		if (next) {
+		return next();
+		} else {
+			return done();
+		}
+
+			
+
+	
+	
+
+	}	catch(error) { 
+
+		return done(error.message);
+
+		// Process Next File
+		if (next) {
+			return next();
+			} else {
+				return done();
+			}
+
 	}
 
-	// Handle errors.
-	catch(error){ console.log(error); }
 }
-
-
-
